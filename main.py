49c75274115079a032d6f87cab51e73790be16aa
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-09-25 21:42:56 +0300

    c

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ num2 = float(input("Введіть друге число: "))
 num3 = float(input("Введіть третє число: "))
 
 print("Оберіть операцію:")
-print("1. Сума трьох чисел")
-print("2. Добуток трьох чисел")
-choice = int(input("Введіть номер операції (1 або 2): "))
+print("1. Максимум")
+print("2. Мінімум")
+print("3. Середньоарифметичне")
+choice = int(input("Введіть номер операції (1, 2 або 3): "))
 
 if choice == 1:
-    result = num1 + num2 + num3
-    print("Сума трьох чисел:", result)
+    result = max(num1, num2, num3)
+    print("Максимум із трьох чисел:", result)
 elif choice == 2:
-    result = num1 * num2 * num3
-    print("Добуток трьох чисел:", result)
+    result = min(num1, num2, num3)
+    print("Мінімум із трьох чисел:", result)
+elif choice == 3:
+    result = (num1 + num2 + num3) / 3
+    print("Середньоарифметичне трьох чисел:", result)
 else:
     print("Некоректний вибір операції")
 
